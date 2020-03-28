b053c1f10d4183035dab23d4948a8f38cfce3def
Nathan T. Baker <nathantbaker@gmail.com>
2020-03-28 15:36:57 -0500

    add full date

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,50 @@
 const now = new Date();
+const monthNumber = now.getMonth();
+let month;
+const day = now.getDate();
+const year = now.getFullYear();
+
+switch (monthNumber) {
+    case 0:
+        month = 'January';
+        break;
+    case 1:
+        month = 'February';
+        break;
+    case 2:
+        month = 'March';
+        break;
+    case 3:
+        month = 'April';
+        break;
+    case 4:
+        month = 'May';
+        break;
+    case 5:
+        month = 'June';
+        break;
+    case 6:
+        month = 'July';
+        break;
+    case 7:
+        month = 'August';
+        break;
+    case 8:
+        month = 'September';
+        break;
+    case 9:
+        month = 'October';
+        break;
+    case 10:
+        month = 'November';
+        break;
+    case 11:
+        month = 'December';
+        break;
+    default:
+        break;
+}
+
 const dayOfWeek = now.getDay();
 switch (dayOfWeek) {
     case 0:
@@ -26,4 +72,6 @@ switch (dayOfWeek) {
         document.write('<h1>Who knows...</h1>');
         break;
 }
+
+document.write(`<h2>${month.toUpperCase()} ${day}, ${year}</h2>`);
 document.write("<a href='https://www.mechanicalloom.com/'><img class='center' id='logo' alt'Mechanical Loom Logo' src='img/mechanical-loom-logo.png'/></a>");
